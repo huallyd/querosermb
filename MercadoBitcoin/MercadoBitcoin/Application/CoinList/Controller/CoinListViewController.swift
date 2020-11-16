9a2ded319875cec9e7ccefb8a6e197e28abf7b52
Huallyd Smadi <huallyd@gmail.com>
2020-11-16 14:04:46 -0300

    Setups initial project

--- MercadoBitcoin/MercadoBitcoin/Application/CoinList/Controller/CoinListViewController.swift
+++ MercadoBitcoin/MercadoBitcoin/Application/CoinList/Controller/CoinListViewController.swift
@@ -25,7 +25,8 @@ final class CoinListViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupView()
+        setupLayout()
     }
     
     // MARK: Private functions
@@ -37,16 +38,5 @@ final class CoinListViewController: BaseViewController {
     private func setupLayout() {
         
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ MercadoBitcoin/Pods/Target Support Files/Pods-MercadoBitcoin/Pods-MercadoBitcoin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MercadoBitcoin : NSObject
+@end
+@implementation PodsDummy_Pods_MercadoBitcoin
+@end
